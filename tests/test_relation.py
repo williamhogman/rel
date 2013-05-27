907f89812f8a520d7bde624cb95876ba6227c55b
William Högman <william@hogman.me>
2013-05-27 11:30:20 +0200

    added tests for key properties

--- tests/test_relation.py
+++ tests/test_relation.py
@@ -74,3 +74,55 @@ class TestRelationAxioms(object):
             """Selecting on a tautology indicated by the python None, in this case
             meaning the empty set of restrictions should be equivalent
             to the identity function""")
+
+
+abc_header = [(int, "a"), (int, "b"), (int, "c")]
+
+class TestKeys(object):
+    @property
+    def ex(self):
+        return r.Relation(abc_header, 
+                          values(("a", "b", "c"), [
+                              (1, 10, 100),
+                              (2, 20, 200),
+                          ]))
+
+    @property
+    def ex_dup(self):
+        return r.Relation(abc_header,
+                          values(["a", "b", "c"], [
+                              (1, 1337, 1337),
+                              (2, 1337, 1337),
+                          ]))
+
+    def test_super_keys(self):
+        rk = list(self.ex.super_keys)
+        eq_(len(rk),
+            2**3,
+            """The number of superkeys for a relation where all attrbibutes are
+            unique should be equal to the square of the order.""")
+
+    def test_single_key_component():
+        sk = list(self.ex_dup.super_keys)
+        
+        ok_(all("a" in sk for k in sk),
+            """In a relation where a single attribute has all other columns
+            depending on it and nothing else. It should be included in
+            all superkeys"""
+        )
+
+    def test_candidate_keys(self):
+        ck = list(sorted(self.ex.candidate_keys))
+        an = list(sorted(set([k]) for k in self.ex.attribute_names))
+        eq_(ck,
+            an,
+            """The candidate keys of a relation where all attributes are unique
+            should be exactly equal to the attribute names""")
+
+    def test_candidate_single_key(self):
+        ck = list(self.ex.candidate_keys)
+        eq(len(ck),
+           1,
+           """There should only be a single candidate key if it is the only
+           unique attribute. All the superkeys (including itself) are
+           therefore supersets of this key.""")
